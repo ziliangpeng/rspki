c59c4d8c01e605287f5ae494c1aa566fd2e06e21
Ziliang Peng <ziliangdotme@gmail.com>
2025-02-01 12:39:16 -0800

    [poc][bash] support different name in key gen

--- poc/bash/generate_key.sh
+++ poc/bash/generate_key.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
+key_name=${1:-test_key}
+key_path="/tmp/.ssh/$key_name"
+pub_path="$key_path.pub"
+if [ -f "$key_path" ]; then
+    echo "Key already exists: $key_path. Deleting..."
+    rm -f "$key_path"
+fi
+if [ -f "$pub_path" ]; then
+    echo "Public key already exists: $pub_path. Deleting..."
+    rm -f "$pub_path"
+fi
 mkdir -p /tmp/.ssh
-echo 'y' | ssh-keygen -t rsa -b 4096 -C "test@test.com" -f /tmp/.ssh/test_key -N ''
+echo 'y' | ssh-keygen -t rsa -b 4096 -C "test@test.com" -f "/tmp/.ssh/$key_name" -N ''
