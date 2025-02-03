c7f16f12f40bf8d602f94fc095f7452f040517b3
Ziliang Peng <ziliangdotme@gmail.com>
2025-02-02 20:25:02 -0800

    compact

--- verify/is_prime.py
+++ verify/is_prime.py
@@ -1,7 +1,5 @@
 from sympy import isprime
 n=input()
-if n.startswith('0x'):
-    n=int(n, 16)
-else:
-    n=int(n)
+if n.startswith('0x'): n=int(n, 16)
+else: n=int(n)
 print(n, 'is prime' if isprime(n) else 'is not prime')
