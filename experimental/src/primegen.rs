476c5fe788bafcb25f9a360587dbf1b437d67e9c
Ziliang Peng <ziliangdotme@gmail.com>
2025-02-09 20:43:14 -0800

    pre filter first 1,000,000 prime factors

--- experimental/src/primegen.rs
+++ experimental/src/primegen.rs
@@ -1,19 +1,7 @@
 mod bigint;
 use bigint::BigInt;
 
-fn generate_prime(n_bits: usize) -> BigInt {
-    let mut n;
-    // Use an infinite loop to generate candidates.
-    loop {
-        n = BigInt::random(n_bits);
-        n |= 1; // Ensure the number is odd.
-        if miller_rabin(&n, 40) {
-            break;
-        }
-    }
-    n
-}
-
+/// Miller-Rabin primality test.
 fn miller_rabin(n: &BigInt, k: u64) -> bool {
     n.assert_valid();
     if n < &2u64 {
@@ -32,7 +20,7 @@ fn miller_rabin(n: &BigInt, k: u64) -> bool {
     let s = d.trailing_zeros();
     d >>= s;
 
-    for _i in 0..k {
+    for _ in 0..k {
         let a = BigInt::random(n.bit_length() - 1);
         n.assert_valid();
         let mut x = a.modpow(&d, n);
@@ -41,19 +29,57 @@ fn miller_rabin(n: &BigInt, k: u64) -> bool {
             continue;
         }
 
-        for _ in 0..(s - 1) {
+        let mut passed_round = false;
+        for _ in 0..((s as usize) - 1) {
             x = x.modpow_u32(2, n);
             if x == n1 {
+                passed_round = true;
                 break;
             }
         }
 
-        if x != n1 {
+        if !passed_round {
             return false;
         }
     }
-
     true
+}
+
+/// Generates a prime with the given bit length.
+fn generate_prime(n_bits: usize, primes: &[u64]) -> BigInt {
+    loop {
+        let mut candidate = BigInt::random(n_bits);
+        candidate |= 1; // Ensure the candidate is odd.
+        if primes.iter().any(|&prime| &candidate % prime == 0) {
+            continue;
+        }
+        if miller_rabin(&candidate, 40) {
+            return candidate;
+        }
+    }
+}
+
+fn generate_first_1000_smallest_primes() -> Vec<u64> {
+    let mut primes = Vec::with_capacity(1000);
+    let mut candidate: u64 = 2;
+    while primes.len() < 1000000 {
+        let mut is_prime = true;
+        let sqrt_candidate = (candidate as f64).sqrt() as u64;
+        for &p in &primes {
+            if p > sqrt_candidate {
+                break;
+            }
+            if candidate % p == 0 {
+                is_prime = false;
+                break;
+            }
+        }
+        if is_prime {
+            primes.push(candidate);
+        }
+        candidate += 1;
+    }
+    primes
 }
 
 fn main() {
@@ -73,8 +99,10 @@ fn main() {
 
     BigInt::seed(42);
     println!("Generating {} primes with {} bits", repeat, n_bits);
+
+    let primes = generate_first_1000_smallest_primes();
     for _ in 0..repeat {
-        let n = generate_prime(n_bits);
-        println!("0x{}", n.hex());
+        let prime = generate_prime(n_bits, &primes);
+        println!("0x{}", prime.hex());
     }
 }
